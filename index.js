425812b87158fb1308eb2b5e251f82226681b399
vivek <vivekchiguru8@gmail.com>
2026-05-23 14:22:35 +0530

    ch

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;  // Must read PORT env var
+
+app.get('/', (req, res) => {
+  res.send('Hello from docker-repo!');
+});
+
+app.listen(port, '0.0.0.0', () => {  // '0.0.0.0' is important
   console.log(`Listening on port ${port}`);
 });
